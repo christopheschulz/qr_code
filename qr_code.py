c255f243c1ec682f2237f4a2f6a1640113066efc
christophe schulz <christophe.schulz@laposte.net>
2025-03-07 09:56:31 +0100

    gestion sauvegarde fichier

--- qr_code.py
+++ qr_code.py
@@ -15,55 +15,64 @@ QR_ERROR_CORRECT_H = qrcode.constants.ERROR_CORRECT_H # 30%
 qr_version = None # maximum 40
 qr_box_size = 10
 qr_border = 4 # minimum 4
-
-# fichier
-p = Path.cwd()
-files = p / "QR" / "some_file.png"
+qr_img_number = 0
 
 
-def get_arguments():
-    arguments = sys.argv[1:]
-
-
-def generate_qr_code(qr_version,qr_error_correction,qr_box_size,qr_border):
+def generate_qr_code(qr_version=1,
+                     qr_error_correction=QR_ERROR_CORRECT_M,
+                     qr_box_size=10,
+                     qr_border=4):
+    
     qr = qrcode.QRCode(
         version=qr_version,
         error_correction=qr_error_correction,
         box_size=qr_box_size,
         border=qr_border,
     )
-    qr.add_data('Some data')
+    qr.add_data('Some data') # entre parenthèses c'est le texte à transformer
     qr.make(fit=True)
 
     img = qr.make_image(fill_color="black", back_color="white")
     ## to do
-    # gestion de  fichiers nom ect..
+    # attention là il faut une gestion de fichier
+    file = file_name_path()
+    img.save(file)
+
+
+def read_qr_code():
+    # read the QRCODE image
+    ## to do
+    # attention là il faut une gestion de fichier
+    file = save_file()
+    img = cv2.imread(file)
+    # initialize the cv2 QRCode detector
+    detector = cv2.QRCodeDetector()
+    # detect and decode
+    data = detector.detectAndDecode(img) 
+    text_qr_code = data[0]
+    print(text_qr_code)   
+
+
+def save_file():
+    p = Path.cwd()
+    qr_folder = p / "QR"
     
-    img.save(files)
+    while Path(file_path).exists():
+        file_name = f"QR{str(qr_img_number).zfill(3)}.png"
+        file_path = qr_folder / file_name
+        qr_img_number += 1
+       
+    return file_path
+
+
+def get_arguments():
+    arguments = sys.argv[1:]
 
 
 def qr_code():
     
     generate_qr_code(qr_version,QR_ERROR_CORRECT_L,qr_box_size,qr_border)
-    read_qr_code()
+    # read_qr_code()
 
-def read_qr_code():
-    # read the QRCODE image
-    img = cv2.imread("site.png")
-    # initialize the cv2 QRCode detector
-    detector = cv2.QRCodeDetector()
-    # detect and decode
-    data, bbox, straight_qrcode = detector.detectAndDecode(img) 
-    # if there is a QR code
-    if bbox is not None:
-        print(f"QRCode data:\n{data}")
-        # display the image with lines
-        # length of bounding box
-        n_lines = len(bbox)
-        for i in range(n_lines):
-            # draw all lines
-            point1 = tuple(bbox[i][0])
-            point2 = tuple(bbox[(i+1) % n_lines][0])
-            cv2.line(img, point1, point2, color=(255, 0, 0), thickness=2)
 
 qr_code()
