a324dea12adb9650f2d87f91191a5424154c0d5d
christophe schulz <christophe.schulz@laposte.net>
2025-03-14 14:46:00 +0100

    lecteur et generatuer fini

--- src/QrCodeReader/views.py
+++ src/QrCodeReader/views.py
@@ -1,7 +1,7 @@
 import os
 from django.conf import settings
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from QrCodeReader.forms import QrGenerateForm,QrLoader
 DIR_QR_CONFIG = "QrCodeReader/config"
 QR_CONFIG_FILE = "config.json"
@@ -13,7 +13,6 @@ def qr_reader(request):
         qr_reader_form = QrLoader(request.POST, request.FILES)
         if qr_reader_form.is_valid():
             qr_img = qr_reader_form.cleaned_data['qr_img']
-            print(qr_img)
 
             upload_dir = os.path.join(settings.MEDIA_ROOT, 'qr_codes')
             os.makedirs(upload_dir, exist_ok=True)
@@ -24,6 +23,7 @@ def qr_reader(request):
                 for chunk in qr_img.chunks():
                     destination.write(chunk)
             image_url = f"{settings.MEDIA_URL}qr_codes/{str(qr_img)}"
+            print(image_url)
 
             result = qr_code.read_qr_code(file_path)
             print(result)
@@ -46,6 +46,7 @@ def qr_generator(request):
             qr_error_correct = int(qr_generate_form.cleaned_data['qr_error_correction_form'])
             qr_box_size = int(qr_generate_form.cleaned_data['qr_box_size_form'])
             
+            # attention le chiemin d'url n'est que correcte en debug
             file_path = qr_code.get_file_path(qr_code.QR_CONFIG_FILE,qr_code.DIR_QR_CONFIG)
             print(file_path)
             qr_code.save_qr_config(file_path,
@@ -54,10 +55,13 @@ def qr_generator(request):
                                    qr_box_size,
                                    qr_border=4)
             qr_code.handle_generate_qr(qr_entry)
+            image_url = f"static/qr_img/QR000.png"
+            print(image_url)
     else:    
         qr_generate_form = QrGenerateForm()
+        image_url = ""
 
-    return render(request,"qr_generator.html",{"form" : qr_generate_form})
+    return render(request,"qr_generator.html",{"form" : qr_generate_form,"image_url":image_url})
 
 
 def qr_history(request):
