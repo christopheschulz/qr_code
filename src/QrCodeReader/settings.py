d9be7dfd1799149fe017977063ae1b3cd481d9e3
christophe schulz <christopheschulz@orange.fr>
2025-08-07 20:28:04 +0200

    fix: Update STATICFILES_DIRS and MEDIA_ROOT paths for correct static and media file handling

--- src/QrCodeReader/settings.py
+++ src/QrCodeReader/settings.py
@@ -127,11 +127,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR,"QrCodeReader/static")
-    ]
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'src/QrCodeReader/static',
+]
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,"media")
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
