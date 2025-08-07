2e26820815c1589f87737662bc847c7348cc4050
christophe schulz <christopheschulz@orange.fr>
2025-08-07 20:08:15 +0200

    Add Tailwind CSS configuration and styles for QR code project
    - Created theme.css with default Tailwind CSS theme variables and colors.
    - Added utilities.css to include Tailwind CSS utilities.
    - Initialized package.json and package-lock.json for project dependencies.
    - Configured tailwind.config.js to specify content paths for Tailwind CSS.
    - Included binary for Tailwind CSS CLI for building styles.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/QrCodeReader/templates/**/*.html",
+    "./src/QrCodeReader/static/**/*.js",
+    "./src/**/*.py",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
